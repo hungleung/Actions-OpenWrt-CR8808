53c4508ebf92149231e7f803f90fd27fbae04b31
hungleung <hungleung.lee@me.com>
2023-08-18 10:02:13 +0800

    Update cr8808.sh

--- cr8808.sh
+++ cr8808.sh
@@ -12,7 +12,7 @@ sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_genera
 sed -i 's/OpenWrt/Home/g' package/base-files/files/bin/config_generate
 # sed -i 's/\+shellsync//' package/network/services/ppp/Makefile
 # sed -i 's/\+kmod-mppe//' package/network/services/ppp/Makefile
-sed -i 's/"ShadowSocksR Plus+"/"SSRP+"/'  feeds/helloworld/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
+# sed -i 's/"ShadowSocksR Plus+"/"SSRP+"/'  feeds/helloworld/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
 
 # disable and remove wireless
 # sed -i 's/\+libiwinfo-lua//' feeds/luci/collections/luci/Makefile
